54edcaf963fc969f0210682321ca91906eef523f
sudheerbk1992 <144927386+sudheerbk1992@users.noreply.github.com>
2023-11-14 00:39:30 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,6 +1,6 @@
 resource "aws_elb" "bar" {
-  name               = "raham-terraform-elb"
-  availability_zones = ["ap-southeast-1a", "ap-southeast-1b"]
+  name               = "sudheer-terraform-elb"
+  availability_zones = ["ap-southeast-4a", "ap-southeast-4b"]
 
   listener {
     instance_port     = 80
@@ -29,17 +29,17 @@ resource "aws_elb" "bar" {
   cluster_identifier      = "aurora-cluster-demo"
   engine                  = "aurora-mysql"
   engine_version          = "5.7.mysql_aurora.2.11.2"
-  availability_zones      = ["ap-southeast-1a", "ap-southeast-1b"]
+  availability_zones      = ["ap-southeast-4a", "ap-southeast-4b"]
   database_name           = "mydb"
-  master_username         = "raham"
-  master_password         = "Rahamshaik#444555"
+  master_username         = "sudheer"
+  master_password         = "bks#444555"
   backup_retention_period = 5
   preferred_backup_window = "07:00-09:00"
 }*/
 
 /*resource "aws_instance" "one" {
   for_each = toset(["one", "two", "three"])
-  ami           = "ami-006be9ab6a140de6e"
+  ami           = "ami-09073b99f17d05c7b"
   instance_type = "t2.micro"
   tags = {
     Name = "instance-${each.key}"
