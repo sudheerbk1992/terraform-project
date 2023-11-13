38d3f16b1aa8ddb8e6a4fbfcd4f40a4937fb3511
sudheerbk1992 <144927386+sudheerbk1992@users.noreply.github.com>
2023-11-14 02:01:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ sudo -i
 yum install httpd -y
 systemctl start httpd
 chkconfig httpd on
-echo "hai all this is my app created by terraform infrastructurte by raham sir server-1" > /var/www/html/index.html
+echo "hai all this is my app created by terraform infrastructurte by sudheer sir server-1" > /var/www/html/index.html
 EOF
   tags = {
     Name = "web-server-1"
